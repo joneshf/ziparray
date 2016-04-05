0f3e8444efa35586a5f215f010c9ecd6f9558a62
joneshf <jones3.hardy@gmail.com>
2016-04-05 08:17:07 -0700

    Begin testing implementation
    In order to have some assurance that we've built the right thing,
    we use property based testing to exercise the laws.

---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const jsv = require('jsverify');
+const R = require('ramda');
+
+const ZipArray = require('.');
+
+const ZipArrayArb = ty => jsv.array(ty).smap(ZipArray, xs => xs.runZipArray);
+const env = {
+  ZipArray: ZipArrayArb,
+};
+
+describe('ZipArray', () => {
+  describe('#map', () => {
+    jsv.property('identity law', 'ZipArray unit', env, xs =>
+      R.equals(xs.runZipArray, xs.map(x => x).runZipArray)
+    );
+  });
+
+  describe('#ap', () => {
+    jsv.property('composition law',
+      'ZipArray (unit -> unit)', 'ZipArray (unit -> unit)', 'ZipArray unit',
+      env, (fs, gs, xs) => {
+        const left = fs.map(f => g => x => f(g(x))).ap(gs).ap(xs).runZipArray;
+        const right = fs.ap(gs.ap(xs)).runZipArray;
+
+        return R.equals(left, right);
+      });
+  });
+});
